175bb7d62f437919c3833c371c7ae4891ee3f760
k33k00 <keewynne90@gmail.com>
2016-09-01 14:08:26 +0100

    csv to python object done. Need to sort out the download of the CSV

--- results.py
+++ results.py
@@ -1,17 +1,20 @@
-import json
 import csv
-import sys, getopt
-import requests
-
+from pprint import pprint
 
 #DrawDate,Ball 1,Ball 2,Ball 3,Ball 4,Ball 5,Lucky Star 1,Lucky Star 2,UK Millionaire Maker,DrawNumber
+results = {}
+with open("results.csv","r") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    row_count = len(data)
+    for i in range(0,row_count):
+        if i != 0:
+            drawDate = data[i][0]
+            balls = [data[i][1],data[i][2],data[i][3],data[i][4],data[i][5]]
+            luckyStars = [data[i][6],data[i][7]]
+            millionaireMaker = data[i][8]
+            drawNumber = data[i][9]
+            results[drawDate] = {"Balls": balls,"Lucky Stars": luckyStars,"Millionaire Maker": millionaireMaker, "Draw Number": drawNumber}
 
 
-#r = requests.get('https://www.national-lottery.co.uk/results/euromillions/draw-history/csv')
-#print(r.content)
-
-
-f = open( 'sample.csv', 'r' )
-reader = csv.DictReader( f, fieldnames = ( "DrawDate","Ball 1","Ball 2","Ball 3","Ball 4", "Ball 5", "Lucky Star 1", "Lucky Star 2", "UK Millionaire Maker", "DrawNumber" ) )
-out = json.dumps( [ row for row in reader ], indent=4 )
-print(out)
+pprint(results)
