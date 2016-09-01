25ef59dcb482e429319c62addf0ddc84157516ea
Kieran Wynne <keewynne90@gmail.com>
2016-09-01 21:37:18 +0100

    getting the results back as a python dict is now a simple function

--- results.py
+++ results.py
@@ -1,20 +1,22 @@
 import csv
 from pprint import pprint
+import urllib2
+import datetime
 
-
-#https://www.national-lottery.co.uk/results/euromillions/draw-history/csv
-
-results = {}
-with open("results.csv","r") as f:
-    reader = csv.reader(f)
+def getResults():
+    r= urllib2.urlopen("https://www.national-lottery.co.uk/results/euromillions/draw-history/csv")
+    results = {}
+    reader = csv.reader(r)
     data = list(reader)
     row_count = len(data)
     for i in range(0,row_count):
         if i != 0:
-            drawDate = data[i][0]
+            drawDate = datetime.datetime.strptime(data[i][0], "%d-%b-%Y").date()
             balls = [data[i][1],data[i][2],data[i][3],data[i][4],data[i][5]]
             luckyStars = [data[i][6],data[i][7]]
             millionaireMaker = data[i][8]
             drawNumber = data[i][9]
             results[drawDate] = {"Balls": balls,"Lucky Stars": luckyStars,"Millionaire Maker": millionaireMaker, "Draw Number": drawNumber}
-pprint(results)
+    return results
+
+pprint(getResults())
