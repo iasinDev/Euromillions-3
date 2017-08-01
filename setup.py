60edc9602fc78f005401f4ed42b0bf02425f36ae
k33k00 <keewynne90+gitlab@gmail.com>
2017-08-01 22:40:36 +0100

    1.0 release

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ setup(
     author_email='keewynne90+github@gmail.com',
     author='Kieran Wynne',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     name='EuroMillions',
     packages=['euromillions',],
     url='https://github.com/k33k00/Euromillions-results',
-    version='0.1',
+    version='1.0',
+    entry_points={
+        'console_scripts': [
+            'euromillions = euromillions.__init__:get_results',
+        ],
+    },
 )
