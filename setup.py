8c67e15ede48a971d2e18487bf6a2e1b6de8aae1
Kieran Wynne <keewynne90@gmail.com>
2017-08-08 10:32:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,4 +15,17 @@ setup(
             'euromillions-latest = Euromillions.euromillions:get_latest',
         ],
     },
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+    ],
 )
