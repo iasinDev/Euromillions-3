bb8eec78508d2ee0486af59cca8c6f86e74f7680
k33k00 <git@k33k00.33mail.com>
2017-08-08 11:34:33 +0100

    Having a readme nightmare

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 import os
 from distutils.core import setup
 
-def _read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 
 setup(
     author_email='keewynne90+github@gmail.com',
     author='Kieran Wynne',
     license='MIT',
-    long_description=_read('README.rst'),
+    long_description=readme(),
     name='EuroMillions',
     packages=['euromillions'],
     url='https://github.com/k33k00/Euromillions-results',
-    version='2.1.3',
+    version='2.1.4',
     entry_points={
         'console_scripts': [
             'euromillions-all = Euromillions.euromillions:get_results',
